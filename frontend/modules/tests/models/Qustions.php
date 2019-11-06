46abddd2c56806eccadc313c7751a96e02a10a58
mgnedash <gnedasch2011@yandex.ru>
2019-11-06 17:59:43 +0300

    init

---
+++ frontend/modules/tests/models/Qustions.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace frontend\modules\tests\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "qustions".
+ *
+ * @property int $id
+ * @property string $text
+ * @property int $tests_id
+ * @property string $position
+ */
+class Qustions extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'qustions';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'tests_id'], 'required'],
+            [['id', 'tests_id'], 'integer'],
+            [['text', 'position'], 'string', 'max' => 45],
+            [['id'], 'unique'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'text' => 'Text',
+            'tests_id' => 'Tests ID',
+            'position' => 'Position',
+        ];
+    }
+}
---
+++ frontend/modules/tests/models/TypeTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace frontend\modules\tests\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "type_test".
+ *
+ * @property int $id
+ * @property string $name
+ */
+class TypeTest extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'type_test';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['id'], 'required'],
+            [['id'], 'integer'],
+            [['name'], 'string', 'max' => 45],
+            [['id'], 'unique'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+        ];
+    }
+}

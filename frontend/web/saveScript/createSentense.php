a238e89eed1534a25a3acf395288368106fbd927
mgnedash <gnedasch2011@yandex.ru>
2019-11-11 15:09:49 +0300

    models

---
+++ frontend/web/saveScript/createSentense.php
@@ -0,0 +1,73 @@
+$(document).on('click',".click_word" , function(e){
+$(e.target).addClass('inResult')
+$('.result').append('<div class="click_word_template inResult">'+ $(e.target).text() + '</div>');
+$(e.target).remove()
+})
+
+
+
+$(document).on('click',".inResult" , function(e){
+$('.info').append('<div class="click_word_template click_word">'+ $(e.target).text() + '</div>');
+$(e.target).remove()
+})
+
+
+$(document).on('click', ".getString", function(e){
+var str = '';
+$(".result .click_word_template").each(function(i, val){
+var text = $.trim($(val).text());
+str = str + ' '+ text
+})
+checkStr(str)
+})
+
+
+
+
+<div class="info">
+    <div class="click_word click_word_template">
+        1 word
+    </div>
+    <div class="click_word click_word_template">
+        2 word
+    </div>
+    <div class="click_word click_word_template">
+        3 word
+    </div>
+    <div class="click_word click_word_template">
+        4 word
+    </div>
+    <div class="click_word click_word_template">
+        5 word
+    </div>
+</div>
+
+
+<a class="getString"
+   data-success-str = "1 word 5 word 4 word 2 word"
+   href="#">
+    Получить строку
+</a>
+
+<div class="result">
+
+</div>
+
+body {
+padding: 20px;
+font-family: Helvetica;
+}
+
+.click_word_template{
+padding: 5px;
+display: inline-block;
+cursor: pointer;
+}
+
+.wrong{
+color: red;
+}
+
+.success{
+color: green;
+}

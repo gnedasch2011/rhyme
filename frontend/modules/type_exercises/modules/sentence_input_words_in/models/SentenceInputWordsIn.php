ad28c6fc59a006cdf3cfa2297a2525014605aa44
mgnedash <gnedasch2011@yandex.ru>
2019-11-22 09:23:45 +0300

    sentence

---
+++ frontend/modules/type_exercises/modules/sentence_input_words_in/models/SentenceInputWordsIn.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace frontend\modules\type_exercises\modules\sentence_input_words_in\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "sentence_input_words_in".
+ *
+ * @property int $id
+ * @property string $name
+ * @property int $group_input_words_in_sent_id
+ * @property int $positon
+ */
+class SentenceInputWordsIn extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'sentence_input_words_in';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name'], 'string'],
+            [['group_input_words_in_sent_id'], 'required'],
+            [['group_input_words_in_sent_id', 'positon'], 'integer'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'group_input_words_in_sent_id' => 'Group Input Words In Sent ID',
+            'positon' => 'Positon',
+        ];
+    }
+
+    public function getWordsForInput()
+    {
+        return $this->hasMany(WordsForInput::className(), ['sentence_input_words_in_id' => 'id']);
+    }
+}

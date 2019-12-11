5a6fa949d24db55aeff092e61f081faf9453f5a5
mgnedash <gnedasch2011@yandex.ru>
2019-12-11 09:38:11 +0300

    input words

--- frontend/widgets/fabric_widget/views/group_test.php
+++ frontend/widgets/fabric_widget/views/group_test.php
@@ -1,7 +1,40 @@
-<?php foreach($model->qustions as $qustions):?>
-<?php
-    echo "<pre>"; print_r($qustions->answers);die();
- ?>
-<?php endforeach;?>
-    
+<?php foreach ($model->qustions as $qustions): ?>
+    <ul>
+        <?php foreach ($qustions->answers as $answer): ?>
+            <a class="item_test" data-id-answers=<?= $answer->id; ?> href="#"><?= $answer->test ;?></a>
 
+        <?php endforeach; ?>
+    </ul>
+<?php endforeach; ?>
+
+
+<?php
+$script = <<< JS
+ var button = $(".item_test")
+
+// handle click and add class
+button.on("click", function(e){
+  e.preventDefault()
+	$(this).toggleClass('item_test_active')
+})	
+
+ 
+JS;
+//маркер конца строки, обязательно сразу, без пробелов и табуляции
+$this->registerJs($script, yii\web\View::POS_READY);
+?>
+
+<style>
+    .item_test {
+        padding: 10px;
+        background: #000;
+        color: white;
+    }
+
+    .item_test_active {
+        padding: 10px;
+        background: green;
+        color: white;
+    }
+
+</style>

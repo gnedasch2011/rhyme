2272f9093cb4ce17e061b0bf37c3e4e1e10e1df8
mgnedash <gnedasch2011@yandex.ru>
2020-09-14 13:51:38 +0300

    horosho

--- frontend/views/rhyme/block/_output_words.php
+++ frontend/views/rhyme/block/_output_words.php
@@ -1,7 +1,12 @@
 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-    <?php foreach ($words as $word): ?>
-        <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
-            <a title="<?= $word['word'] ?? ''; ?>" href="<?= $word['url'] ?? ''; ?>" class="accent"><?= $word['word'] ?? ''; ?></a>
-        </div>
-    <?php endforeach; ?>
+    <ul>
+        <?php foreach ($words as $word): ?>
+            <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                <li>
+                    <a title="<?= $word['word'] ?? ''; ?>" href="<?= $word['url'] ?? ''; ?>"
+                       class="accent"><?= $word['word'] ?? ''; ?></a>
+                </li>
+            </div>
+        <?php endforeach; ?>
+    </ul>
 </div>

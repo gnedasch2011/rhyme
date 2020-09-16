eb286738bbaec750a6cfc110bbd9e346d4e3ce45
mgnedash <gnedasch2011@yandex.ru>
2020-09-16 13:04:38 +0300

    url migrations

---
+++ frontend/modules/url/components/SearchCityUrlRule.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 2000
+ * Date: 03.08.2020
+ * Time: 16:31
+ */
+
+namespace frontend\modules\url\components;
+
+use app\models\residentname\Url;
+use yii\web\UrlRuleInterface;
+use yii\base\BaseObject;
+
+class SearchCityUrlRule extends BaseObject implements UrlRuleInterface
+{
+
+    public function createUrl($manager, $route, $params)
+    {
+        return false;  // данное правило не применимо
+    }
+
+    public function parseRequest($manager, $request)
+    {
+        $pathInfo = $request->getPathInfo();
+        $pattern = "%([a-zA-Z0-9-]+)?$%";
+
+        if (preg_match($pattern, $pathInfo, $matches)) {
+
+            if (count($matches) != 2) { // условие для выхода (Примечание 1)
+                return false;
+            }
+
+            if (($url = Url::find()
+                    ->where([
+                        'url' => $matches[0],
+                        'route' => ['search-city']
+                    ])
+                    ->one()) == NULL) { // (Примечание 2)
+                return false;
+            }
+
+            return ['/residentname/search-city-by-spell', [ // (Примечание 3)
+                'url' => $url
+            ]];
+        }
+        return false; //(Примечание 4)
+    }
+}
